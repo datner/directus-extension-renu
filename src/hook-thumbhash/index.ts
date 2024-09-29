d9c239eb38355b40017a8756b0a2d719e9231490
Yuval Datner <22598347+datner@users.noreply.github.com>
2024-09-29 03:48:05 +0300

    change to sharp

--- src/hook-thumbhash/index.ts
+++ src/hook-thumbhash/index.ts
@@ -1,39 +1,9 @@
 import type { AssetsService, ItemsService } from "@directus/api/dist/services";
-import type { TransformationParams } from "@directus/api/dist/types";
 import { defineHook } from "@directus/extensions-sdk";
 import type { File } from "@directus/types";
 import { rgbaToThumbHash } from "thumbhash";
+const sharp = require("sharp");
 
-function resizeImage(
-  originalWidth: number,
-  originalHeight: number,
-  maxWidth: number = 100,
-  maxHeight: number = 100,
-): { width: number; height: number } {
-  // Calculate the aspect ratio
-  const aspectRatio = originalWidth / originalHeight;
-
-  // Initialize new dimensions
-  let newWidth = originalWidth;
-  let newHeight = originalHeight;
-
-  // Check if the width exceeds the maximum width
-  if (newWidth > maxWidth) {
-    newWidth = maxWidth;
-    newHeight = newWidth / aspectRatio;
-  }
-
-  // Check if the height exceeds the maximum height
-  if (newHeight > maxHeight) {
-    newHeight = maxHeight;
-    newWidth = newHeight * aspectRatio;
-  }
-
-  return {
-    width: Math.round(newWidth),
-    height: Math.round(newHeight),
-  };
-}
 export default defineHook(({ action }, ctx) => {
   const services = ctx.services as {
     ItemsService: typeof ItemsService;
@@ -58,26 +28,21 @@ export default defineHook(({ action }, ctx) => {
       knex: database,
     });
 
-    const transformationParams: TransformationParams = {
-      withoutEnlargement: true,
-      fit: "inside",
-      format: "jpg",
-      quality: 30,
-      width: 100,
-      height: 100,
-    };
+    const result = await assetService.getAsset(key);
 
-    const result = await assetService.getAsset(
-      key,
-      { transformationParams },
-    );
+    const { data, info } = await result.stream.pipe(
+      sharp()
+        .resize({
+          width: 100,
+          height: 100,
+          fit: "inside",
+        }).ensureAlpha().raw(),
+    )
+      .toBuffer({
+        resolveWithObject: true,
+      });
 
-    const file = result.file as File;
-
-    const contents = await result.stream.toArray().then(Buffer.concat);
-    const meta = resizeImage(file.width!, file.height!);
-
-    const bytes = rgbaToThumbHash(meta.width, meta.height, contents);
+    const bytes = rgbaToThumbHash(info.width, info.height, data);
 
     await sudoService.updateOne(key, {
       thumbhash: Buffer.from(bytes).toString("base64"),
